3356e79682aad50bd6e12f7e77848f1377edb268
Erik Zachrisson <erik@zachrisson.info>
2014-01-19 14:22:15 +0100

    Connect ack err to dot led

--- SccbMasterHack.vhd
+++ SccbMasterHack.vhd
@@ -40,12 +40,15 @@ architecture fpga of SccbMaster is
 	signal ClkCnt_N, ClkCnt_D         : word(bits(ClkWrap)-1 downto 0);
 	signal ClkFlop_N, ClkFlop_D       : bit1;
 	
+	signal re_n, re_d : bit1;
+	
 	signal DataPulse : bit1;
 	signal rw_i_n, rw_i_d : bit1;
 	signal Busy_N, Busy_D : bit1;
 	signal data_i : word(16-1 downto 0);
 	signal valid_i : bit1;
 	signal trans_done : bit1;
+	signal ack_err : bit1;
 	
 	component SCCBCtrl
 	port (
@@ -80,13 +83,13 @@ begin
 		data_o => DataFromSccb,
 		rw_i => rw_i_d,
 		start_i => Busy_D,
-		ack_error_o => valid_i,
+		ack_error_o => ack_err,
 		done_o => trans_done,
 		sioc_o => SIO_C,
 		siod_io => SIO_D
 	);
 	
-	valid <= not trans_done;
+	valid <= not ack_err;
 	
 	ClkDivSync : process (Clk, Rst_N)
 	begin
@@ -95,40 +98,45 @@ begin
 			ClkFlop_D <= '0';
 			Busy_D <= '0';
 			rw_i_d <= '0';
+			re_d <= '0';
 		elsif rising_edge(Clk) then
 			ClkCnt_D  <= ClkCnt_N;
 			ClkFlop_D <= ClkFlop_N;
 			Busy_D <= Busy_N;
 			rw_i_d <= rw_i_n;
+			re_d <= re_n;
 		end if;
 	end process;
 
-	FSMAsync : process (ClkCnt_D, ClkFlop_D, trans_done, Busy_D, We, Re, rw_i_d)
-	begin
-		DataPulse <= '0';
-		
+	DataPulse <= '1' when ClkFlop_D = '0' and ClkCnt_D = DataWrap else '0';
+	
+	FSMAsync : process (ClkCnt_D, ClkFlop_D, trans_done, Busy_D, We, Re, rw_i_d, DataPulse, re_d)
+	begin		
 		ClkFlop_N <= ClkFlop_D;
 		ClkCnt_N  <= ClkCnt_D + 1;
 		Busy_N <= Busy_D;
 		rw_i_n <= rw_i_d;
+		re_n <= re_d;
 		
-		if (((We or Re) = '1') and Busy_D = '0') then
-			Busy_N <= '1';
-			rw_i_n <= We;
-		end if;
-		
-		if (Busy_D = '1' and trans_done = '1') then
-			Busy_N <= '0';
-		end if;
+		if re = '1' then
+			re_n <= '1';
+		end if;	
 
 		if (ClkCnt_D = ClkWrap) then
 			ClkCnt_N  <= (others => '0');
 			ClkFlop_N <= not ClkFlop_D;
 		end if;
 		
-		-- Generate data event on rising serial clock edge
-		if (ClkFlop_D = '0' and ClkCnt_D = DataWrap) then
-			DataPulse <= '1';
+		if DataPulse = '1' then
+			if ((re_d = '1') and Busy_D = '0') then
+				Busy_N <= '1';
+				rw_i_n <= We;
+			end if;
+
+			if (Busy_D = '1' and trans_done = '1') then
+				Busy_N <= '0';
+			end if;
+			re_n <= '0';
 		end if;
 	end process;
 end architecture;
