6a12a1d13119d775cada49bf6c70b392f6863b09
Erik Zachrisson <erik@zachrisson.info>
2014-01-19 09:46:11 +0100

    Add proper thresholding and increase addr to 8

--- SccbAddrReader.vhd
+++ SccbAddrReader.vhd
@@ -8,7 +8,7 @@ use work.Types.all;
 entity SccbAddrReader is 
 	generic (
 		DataW : positive := 8;
-		AddrW : positive := 7;
+		AddrW : positive := 8;
 		-- 
 		ClkFreq : positive := 25000000
 	);	
@@ -26,24 +26,33 @@ end entity;
 
 architecture rtl of SccbAddrReader is
 	signal Addr_N, Addr_D : word(AddrW-1 downto 0);
+	signal LastInc_N, LastInc_D : bit1;
+	signal LastDec_N, LastDec_D : bit1;
 begin
 	SyncProc : process (RstN, Clk)
 	begin
 		if RstN = '0' then
 			Addr_D <= (others => '0');
+			LastInc_D <= '1';
+			LastDec_D <= '1';
 		elsif rising_edge(Clk) then
 			Addr_D <= Addr_N;
+			LastInc_D <= LastInc_N;
+			LastDec_D <= LastDec_N;
 		end if;
 	end process;
 	
-	AsyncProc : process (IncAddr, DecAddr, Addr_D)
+	AsyncProc : process (IncAddr, DecAddr, Addr_D, LastInc_D, LastDec_D)
 	begin
 		Addr_N <= Addr_D;
+		LastInc_N <= IncAddr;
+		LastDec_N <= DecAddr;
 		Re <= '0';
-		if IncAddr = '1' then
+
+		if IncAddr = '0' and LastInc_D = '1' then
 			Addr_N <= Addr_D + 1;
 			Re <= '1';
-		elsif DecAddr = '1' then
+		elsif DecAddr = '0' and LastDec_D = '1' then
 			Addr_N <= Addr_D - 1;
 			Re <= '1';
 		end if;
