83050db6914a9752a3e5b0d156650cfc9318c697
Erik Zachrisson <erik@zachrisson.info>
2014-01-24 22:05:36 +0100

    Remove local addr and data flops

--- SccbMasterNextGen.vhd
+++ SccbMasterNextGen.vhd
@@ -19,7 +19,7 @@ entity SccbMaster is
 		Addr         : in word(AddrW-1 downto 0);
 		We           : in bit1;
 		Re           : in bit1;
-		DataToSccb   : in word(DataW-1 downto 0);
+		Data         : in word(DataW-1 downto 0);
 		DataFromSccb : out word(DataW-1 downto 0);
 		Valid        : out bit1;
 		--
@@ -48,7 +48,6 @@ architecture fpga of SccbMaster is
 	signal Re_N, Re_D : bit1;
 	signal Ack_N, Ack_D : word3;
 	signal ClkGate_N, ClkGate_D : bit1;
-	signal Addr_N, Addr_D : word(8-1 downto 0);
 	signal OpenClkGate_N, OpenClkGate_D : bit1;
 	
 	type SccbStates is (IDLE,
@@ -67,7 +66,6 @@ begin
 			LocStateCnt_D <= (others => '0');
 			ClkGate_D <= '1';
 			Ack_D <= (others => '1');
-			Addr_D <= (others => '0');
 			OpenClkGate_D <= '0';
 		elsif rising_edge(Clk) then
 			ClkCnt_D  <= ClkCnt_N;
@@ -77,7 +75,6 @@ begin
 			LocStateCnt_D <= LocStateCnt_N;
 			ClkGate_D <= ClkGate_N;
 			Ack_D <= Ack_N;
-			Addr_D <= Addr_N;
 			OpenClkGate_D <= OpenClkGate_N;
 		end if;
 	end process;
@@ -86,7 +83,7 @@ begin
 	Valid <= Ack_D(0);
 	DataFromSccb <= (others => '0');
 
-	FSMAsync : process (ClkCnt_D, ClkFlop_D, We, Re, Re_D, LocStateCnt_D, ClkGate_D, Ack_D, SccbState_D, SIO_D, Addr_D, Addr, OpenClkGate_D)
+	FSMAsync : process (ClkCnt_D, ClkFlop_D, We, Re, Re_D, LocStateCnt_D, ClkGate_D, Ack_D, SccbState_D, SIO_D, Addr, OpenClkGate_D, Data)
 		variable SccbEvent : boolean;
 	begin		
 		ClkFlop_N <= ClkFlop_D;
@@ -98,15 +95,13 @@ begin
 		SIO_D <= 'Z';
 		ClkGate_N <= ClkGate_D;
 		Ack_N  <= Ack_D;
-		Addr_N <= Addr_D;
 		OpenClkGate_N <= OpenClkGate_D;
 
 		if (ClkCnt_D = ClkWrap) then
 			ClkCnt_N  <= (others => '0');
-			ClkFlop_N <= not ClkFlop_D;
-			if OpenClkGate_D = '1' then
-				OpenClkGate_N <= '0';
-				ClkGate_N <= '0';
+			ClkFlop_N <= not ClkFlop_D;	
+			if ClkFlop_D = '1' then 
+				ClkGate_N <= not OpenClkGate_D;
 			end if;
 		end if;
 		
@@ -118,13 +113,12 @@ begin
 		when IDLE =>
 			Re_N <= '0';
 			LocStateCnt_N <= (others => '0');
-			ClkGate_N <= '1';
+			OpenClkGate_N <= '0';
 
-			if (Re = '1') then
+			if ((Re or We) = '1') then
 				ClkCnt_N <= (others => '0');
-				Addr_N <= Addr;
 				SccbState_N <= WRITE_PHASE_1;
-				Re_N <= '1';
+				Re_N <= Re;
 			end if;
 
 		when WRITE_PHASE_1 =>
@@ -164,18 +158,16 @@ begin
 				SIO_D <= WriteBit;
 
 			when "1011" =>
-				-- SIO_D <= '0'; -- N/A
 				Ack_N(0) <= SIO_D;
 	
 			when others =>
-				SIO_D <= '0';
-
+				null;
 			end case;
 	
 			if SccbEvent then
 				LocStateCnt_N <= LocStateCnt_D + 1;
-				if (LocStateCnt_D > "1011") then
-					SccbState_N <= IDLE;
+				if (LocStateCnt_D >= "1011") then
+					SccbState_N <= WRITE_PHASE_2;
 					LocStateCnt_N <= (others => '0');
 				end if;
 			end if;
@@ -183,44 +175,93 @@ begin
 		when WRITE_PHASE_2 =>
 			case LocStateCnt_D is
 			when "0000" =>
-				SIO_D <= Addr_D(7);
+				SIO_D <= Addr(7);
 			
 			when "0001" =>
-				SIO_D <= Addr_D(6);
+				SIO_D <= Addr(6);
 				
 			when "0010" =>
-				SIO_D <= Addr_D(5);
+				SIO_D <= Addr(5);
 				
 			when "0011" =>
-				SIO_D <= Addr_D(4);
+				SIO_D <= Addr(4);
 				
 			when "0100" =>
-				SIO_D <= Addr_D(3);
+				SIO_D <= Addr(3);
 				
 			when "0101" =>
-				SIO_D <= Addr_D(2);
+				SIO_D <= Addr(2);
 				
 			when "0110" =>
-				SIO_D <= Addr_D(1);
+				SIO_D <= Addr(1);
 				
 			when "0111" =>
-				SIO_D <= Addr_D(0);
+				SIO_D <= Addr(0);
 				
 			when "1000" =>
 				Ack_N(1) <= SIO_D;
-	
+				
 			when others =>
-				SIO_D <= '0';
+				null;
 
 			end case;
 			
 			if SccbEvent then 
-				if LocStateCnt_D > "1000" then
+				LocStateCnt_N <= LocStateCnt_D + 1;
+				if LocStateCnt_D >= "1000" then
 					if Re_D = '1' then
 						SccbState_N <= READ_PHASE_1;
 					else
-						SccbState_N <= WRITE_PHASE_2;
+						SccbState_N <= WRITE_PHASE_3;
 					end if;
+				end if;
+			end if;
+			
+		when WRITE_PHASE_3 =>
+			case LocStateCnt_D is
+			when "0000" =>
+				SIO_D <= Data(7);
+			
+			when "0001" =>
+				SIO_D <= Data(6);
+				
+			when "0010" =>
+				SIO_D <= Data(5);
+				
+			when "0011" =>
+				SIO_D <= Data(4);
+				
+			when "0100" =>
+				SIO_D <= Data(3);
+				
+			when "0101" =>
+				SIO_D <= Data(2);
+				
+			when "0110" =>
+				SIO_D <= Data(1);
+				
+			when "0111" =>
+				SIO_D <= Data(0);
+				
+			when "1000" =>
+				Ack_N(2) <= SIO_D;
+				
+			when "1001" =>
+				SIO_D <= '0';
+				OpenClkGate_N <= '0';
+			
+			when "1010" =>
+				SIO_D <= '1';
+				
+			when others =>
+				null;
+
+			end case;
+			
+			if SccbEvent then
+				LocStateCnt_N <= LocStateCnt_D + 1;
+				if LocStateCnt_D >= "1010" then
+					SccbState_N <= IDLE;
 				end if;
 			end if;
 				
