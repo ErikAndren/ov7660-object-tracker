340565387fe323094f1cd5fbec2eedda7b64ad82
Erik Zachrisson <erik@zachrisson.info>
2014-01-25 22:58:18 +0100

    Map data bus

--- OV76X0Top.vhd
+++ OV76X0Top.vhd
@@ -16,7 +16,12 @@ entity OV76X0 is
 	Button1 : in bit1;
 	Button2 : in bit1;
 	--
-	PllClk : out bit1;
+	VSYNC : in bit1;
+	HREF : in bit1;
+	--
+	XCLK : out bit1;
+	PCLK : in  bit1;
+	D    : in  word(8-1 downto 0);
 	-- Lcd Interface
 	Display : out word(Displays-1 downto 0);
 	Segments : out word(8-1 downto 0);
@@ -36,7 +41,7 @@ architecture rtl of OV76X0 is
 	signal SccbRe   : bit1;
 	signal SccbWe   : bit1;
 	signal SccbAddr : word(SccbAddrW-1 downto 0);
-	signal PllClk_i : bit1;
+	signal XCLK_i : bit1;
 	signal RstN : bit1;
 
 begin
@@ -44,28 +49,28 @@ begin
 	Pll : entity work.Pll
 	port map (
 		inclk0 => Clk,
-		c0     => PllClk_i
+		c0     => XCLK_i
 	);
-	PllClk <= PllClk_i;
+	XCLK <= XCLK_i;
 	
 	RstSync : entity work.ResetSync
 	port map (
 		AsyncRst => AsyncRstN,
-		Clk      => PllClk_i,
+		Clk      => XCLK_i,
 		--
 		Rst_N    => RstN
 	);
 	
 	DebBtn1 : entity work.Debounce
 	port map (
-		Clk => PllClk_i,
+		Clk => XCLK_i,
 		x   => Button1,
 		DBx => Btn1Stab
 	);
 	
 	DebBtn12 : entity work.Debounce
 	port map (
-		Clk => PllClk_i,
+		Clk => XCLK_i,
 		x   => Button2,
 		DBx => Btn2Stab
 	);
@@ -76,7 +81,7 @@ begin
 	   Freq => Freq
 	)
 	port map (
-		Clk => PllClk_i,
+		Clk => XCLK_i,
 		RstN => AsyncRstN,
 		--
 		Data => LcdDisp,
@@ -93,7 +98,7 @@ begin
 		ClkFreq => Freq
 	)
 	port map (
-		Clk          => PllClk_i,
+		Clk          => XCLK_i,
 		Rst_N        => RstN,
 		--
 		DataFromSccb => SccbData,
