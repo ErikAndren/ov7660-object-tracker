5e730352ec37d354197485f2131da6dee1040f62
Erik Zachrisson <erik@zachrisson.info>
2014-03-21 20:56:50 +0100

    Update pack

--- OV76X0Pack.vhd
+++ OV76X0Pack.vhd
@@ -6,47 +6,50 @@ use ieee.std_logic_unsigned.all;
 use work.Types.all;
 
 package OV76X0Pack is
-	constant SccbAddrW : positive := 8;
-	constant SccbDataW : positive := 8;
+  constant SccbAddrW : positive := 8;
+  constant SccbDataW : positive := 8;
 
-	constant SramDataW : positive := 16;
-	constant SramAddrW : positive := 18;
+  constant SramDataW : positive := 16;
+  constant SramAddrW : positive := 18;
 
-	constant PixelResW : positive := 3;
-	
-	constant NoPixels : positive := SramDataW / PixelResW;
-	constant NoPixelsW : positive := bits(NoPixels);
-	
-	constant NoBuffers : positive := 2;
-	constant NoBuffersW : positive := bits(NoBuffers);
-	
-	constant BufferAddrOffs : positive := 16#10000#;
-	
-	constant FrameW : positive := 640;
-	constant FrameH : positive := 480;
-	constant FrameHW : positive := bits(FrameH);
-	constant FrameRes : positive := FrameW * FrameH;
-	
-	constant MemWordsPerLine : positive := FrameW / NoPixels;
-	constant MemWordsPerLineW : positive := bits(MemWordsPerLine);
-	
-	constant InstPtrW : positive := 4;
+  constant PixelResW : positive := 3;
 
-   constant tClk  : positive := 1;
-   constant tP    : positive := 2 * tClk;
-   constant tLine : positive := tP * 784;
+  constant NoPixels  : positive := SramDataW / PixelResW;
+  constant NoPixelsW : positive := bits(NoPixels);
 
-   constant tVsyncPeriod : positive := tLine * 510;
-   constant tVsyncHigh   : positive := 4;
+  constant NoBuffers  : positive := 2;
+  constant NoBuffersW : positive := bits(NoBuffers);
 
-   constant tHrefPreamble  : positive := tVsyncHigh + 11;
-   constant tHrefPostamble : positive := 15;
-   constant noHrefs        : positive := 480;
+  constant BufferAddrOffs : positive := 16#10000#;
 
-   constant tHrefHigh   : positive := 640 * tP;
-   constant tHrefLow    : positive := 144 * tP;
-   constant tHrefPeriod : positive := tHrefHigh + tHrefLow;
+  constant FrameW   : positive := 640;
+  constant FrameH   : positive := 480;
+  constant FrameHW  : positive := bits(FrameH);
+  constant FrameRes : positive := FrameW * FrameH;
 
+  constant MemWordsPerLine  : positive := FrameW / NoPixels;
+  constant MemWordsPerLineW : positive := bits(MemWordsPerLine);
+
+  constant InstPtrW : positive := 4;
+
+  constant tClk  : positive := 1;
+  constant tP    : positive := 2 * tClk;
+  constant tLine : positive := tP * 784;
+
+  constant tVsyncPeriod : positive := tLine * 510;
+  constant tVsyncHigh   : positive := 4;
+
+  constant tHrefPreamble  : positive := tVsyncHigh + 11;
+  constant tHrefPostamble : positive := 15;
+  constant noHrefs        : positive := 480;
+
+  constant tHrefHigh   : positive := 640 * tP;
+  constant tHrefLow    : positive := 144 * tP;
+  constant tHrefPeriod : positive := tHrefHigh + tHrefLow;
+
+  constant PixelW : positive := 8;
+  type PixVec is array (natural range <>) of word(PixelW-1 downto 0);
+  
 end package;
 
 package body OV76X0Pack is
