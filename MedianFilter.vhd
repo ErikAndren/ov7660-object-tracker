e4e645046e34fdb8d47b0277a1f4c041f9cd2b07
Erik Zachrisson <erik@zachrisson.info>
2014-04-13 13:35:58 +0200

    Reclock to meet 20 ns setup timing

--- MedianFilter.vhd
+++ MedianFilter.vhd
@@ -35,6 +35,7 @@ architecture rtl of MedianFilter is
   constant Threshold                  : natural := 255;
   signal PixelOut_N, PixelOut_D       : word(DataW-1 downto 0);
   signal PixelOutVal_N, PixelOutVal_D : bit1;
+  signal PixelOutVal_D2 : bit1;
 
   procedure Comparator (signal X : in word; signal Y : in word; signal Higher : out word; signal Lower : out word) is
   begin
@@ -51,7 +52,9 @@ architecture rtl of MedianFilter is
   signal G_H, G_L, H_H, H_L, I_H, I_L, J_H, J_L, K_H, K_L, L_H, L_L : word(PixelW-1 downto 0);
   signal M_H, M_L, N_H, N_L, O_H, O_L, Q_H, Q_L, S_H, S_L, T_H, T_L : word(PixelW-1 downto 0);
   signal U_H, U_L, Median                                           : word(PixelW-1 downto 0);
-
+  --
+  signal J_L_D, I_H_D, K_H_D, K_L_D, I_L_D, D_L_D, L_H_D            : word(PixelW-1 downto 0);
+  
 begin
   A : Comparator(PixelIn(0)(0), PixelIn(0)(1), A_H, A_L);
   B : Comparator(PixelIn(1)(0), PixelIn(1)(1), B_H, B_L);
@@ -69,11 +72,11 @@ begin
   K : Comparator(G_L, H_L, K_H, K_L);
   L : Comparator(E_L, F_L, L_H, L_L);
   --
-  M : Comparator(J_L, I_H, M_H, M_L);
-  N : Comparator(K_L, I_L, N_H, N_L);
-  O : Comparator(D_L, L_H, O_H, O_L);
+  M : Comparator(J_L_D, I_H_D, M_H, M_L);
+  N : Comparator(K_L_D, I_L_D, N_H, N_L);
+  O : Comparator(D_L_D, L_H_D, O_H, O_L);
   --
-  Q : Comparator(K_H, N_H, Q_H, Q_L);
+  Q : Comparator(K_H_D, N_H, Q_H, Q_L);
   S : Comparator(M_L, Q_L, S_H, S_L);
   T : Comparator(S_L, O_H, T_H, T_L);
   --
@@ -83,29 +86,34 @@ begin
   SyncRstProc : process (Clk, RstN)
   begin
     if RstN = '0' then
-      PixelOutVal_D <= '0';
+      PixelOutVal_D  <= '0';
+      PixelOutVal_D2 <= '0';
     elsif rising_edge(Clk) then
-      PixelOutVal_D <= PixelOutVal_N;
+      PixelOutVal_D  <= PixelOutVal_N;
+      PixelOutVal_D2 <= PixelOutVal_D;
     end if;
   end process;
 
   SyncNoRstProc : process (Clk)
   begin
     if rising_edge(Clk) then
-      PixelOut_D    <= PixelOut_N;
+      PixelOut_D <= PixelOut_N;
+      J_L_D      <= J_L;
+      I_H_D      <= I_H;
+      K_H_D      <= K_H;
+      K_L_D      <= K_L;
+      I_L_D      <= I_L;
+      D_L_D      <= D_L;
+      L_H_D      <= L_H;
     end if;
   end process;
   
   AsyncProc : process (PixelIn, PixelInVal, Median)
   begin
     PixelOutVal_N <= PixelInVal;
-    PixelOut_N    <= (others => '0');
-
-    if PixelInVal = '1' then
-      PixelOut_N <= Median;
-    end if;
+    PixelOut_N    <= Median;
   end process;
 
   PixelOut    <= PixelOut_D;
-  PixelOutVal <= PixelOutVal_D;
+  PixelOutVal <= PixelOutVal_D2;
 end architecture rtl;
